6995ee01c7b114426b1bc9a601b3c073988398bf
Talia Aziz Khan <talyaaziz@gmail.com>
2021-10-19 06:32:24 +0000

    a few tinkerings

---
+++ tinkering.js
@@ -0,0 +1,22 @@
+function multiply(a, b) {
+return a * b;
+}
+const result = multiply(2, 4);
+console.log(result);
+
+/*
+ * Programming Quiz: First Expression (2-1)
+ * Write an expression that uses at least three, distinct, arithmetic operators
+ * to log the number 42 to the console.
+ */
+ 
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should print the value `42`
+ * 2. You should use at least 3 distinct operators. (`+`, `-`, `*`, `/`, or `%`)
+ * 3. Your code should not be empty
+ */
+
+
+// this expression equals 4, change it to equal 42
+console.log(1 + 5 - 2);
