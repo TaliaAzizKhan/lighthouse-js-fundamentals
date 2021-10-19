c539c59282b23ffc2b5cc6e0c1e12f784c36bb91
Talia Aziz Khan <talyaaziz@gmail.com>
2021-10-19 06:17:52 +0000

    some more loops

---
+++ loopylighthouse.js
@@ -0,0 +1,31 @@
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+ 
+ /*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an `emotions()` function
+ * - Your code should call the `emotions()` function
+ * - Your `emotions()` function call should have an inline function expression passed as the second parameter
+ * - Your function expression should return the expected output
+ */
+
+
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression
+emotions("happy", function laugh(num) {
+  s = "";
+  for (let i = 0; i < num; i++) {
+    s = s + "ha";
+  }
+  return s + "!";
+})
+
